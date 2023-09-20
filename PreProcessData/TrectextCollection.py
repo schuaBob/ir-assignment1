04e52985083bfaadc04b7c384478c1fc78b79a77
Bob Hua <suh46@pitt.edu>
2023-09-20 07:26:03 -0400

    finish TrectextCollection, uncleaned

--- PreProcessData/TrectextCollection.py
+++ PreProcessData/TrectextCollection.py
@@ -10,32 +10,48 @@ class TrectextCollection:
         # 2. Make preparation for function nextDocument().
         # NT: you cannot load the whole corpus into memory!!
         self.__file = open(Path.DataTextDir, "r")
-        self.docInfo = self.__prepare_file()
+        self.__docInfo = self.__prepare_file()
         return
 
     def __prepare_file(self):
-        contentStart, contentLines = 0, 0
+        offset, totalLines = 0, 0
+        offset2 = 0
+        # to be removed
         i = 0
         while line := self.__file.readline():
+            # to be removed
+            if i == 3:
+                break
             if "<DOC>" in line:
-                contentStart = self.__file.tell()
+                offset = self.__file.tell()
                 continue
 
             if "</DOC>" in line:
-                yield (contentStart, contentLines)
-                contentLines = 0
-                i+=1
+                i += 1
+                offset2 = self.__file.tell()
+                yield (offset, offset2 - len(line))
+                totalLines = 0
+                self.__file.seek(offset2)
+                # to be removed
             else:
-                contentLines += 1
-
+                totalLines += 1
 
     def nextDocument(self):
         # 1. When called, this API processes one document from corpus, and returns its doc number and content.
         # 2. When no document left, return null, and close the file.
-        
-        docNo = ""
-        content = ""
-        return [docNo, content]
+        if docInfo := next(self.__docInfo, None):
+            offset, endbyte = docInfo
+            self.__file.seek(offset)
+            doc = self.__file.read(endbyte - offset)
+            docNo = self.__get_tag_content("DOCNO", doc)
+            content = self.__get_tag_content("TEXT", doc)
+            return [docNo, content]
+        else:
+            self.__file.close()
+            return docInfo
 
-    def __del__(self):
-        self.__file.close()
+    def __get_tag_content(self, tag: str, doc: str):
+        openTag, closeTag = f"<{tag}>", f"</{tag}>"
+        return doc[
+            (start := doc.find(openTag) + len(openTag)) : doc.find(closeTag, start)
+        ].strip()
